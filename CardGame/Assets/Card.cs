5006a22acb5020ba5bf2067b78848e1226fd4ae4
amoskao <51389193+amoskao@users.noreply.github.com>
2019-06-12 16:31:09 +0800

    新增卡牌藍圖

---
+++ CardGame/Assets/Card.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+
+public class Card
+{
+    private int _cardnumber;
+    private float _cardposistion;
+    private Sprite _cardimg;
+
+    public int CardNumber { get => _cardnumber;}
+    public float CardPosistion { get => _cardposistion; set => _cardposistion = value; }
+    public Sprite CardImg { get => _cardimg; }
+    
+    public Card(int num, float loc, Sprite img)
+    {
+        _cardnumber = num;
+        _cardposistion = loc;
+        _cardimg = img;
+    }
+}
