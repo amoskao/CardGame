17cab2952911913a173404185491c008972d654c
amoskao <51389193+amoskao@users.noreply.github.com>
2019-06-20 10:19:48 +0800

    新增15點場景

---
+++ CardGame/Assets/FifteenManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FifteenManager : MonoBehaviour
+{
+    public Card PokerPlayer, PokerCOM;
+    public Sprite[] CardImage;
+    public TextMesh tip;
+
+    private void Start()
+    {
+        int Pnum1 = Random.Range(0, 16);
+        PokerPlayer = new Card(Pnum1, -2, CardImage[Pnum1]);
+    }
+}
