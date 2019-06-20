097dcf27dea390bf0056df6fea063feacde97600
amoskao <51389193+amoskao@users.noreply.github.com>
2019-06-20 12:42:02 +0800

    加入兩張卡片

--- CardGame/Assets/FifteenManager.cs
+++ CardGame/Assets/FifteenManager.cs
@@ -1,14 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FifteenManager : MonoBehaviour
 {
-    public Card PokerPlayer, PokerCOM;
+    public Card[] PokerPlayer = new Card[2];
+    public Card[] PokerCOM = new Card[2];
     public Sprite[] CardImage;
     public TextMesh tip;
 
+    private void MakeCard(Card card, int x, int num)
+    {
+        if (num <= 10)
+            card = new Card(num, x, CardImage[num - 1]);
+        else
+            card = new Card(10, x, CardImage[num - 1]);
+    }
+
     private void Start()
     {
-        int Pnum1 = Random.Range(0, 16);
-        PokerPlayer = new Card(Pnum1, -2, CardImage[Pnum1]);
+        MakeCard(PokerPlayer[0], -2, Random.Range(1, 13));
+        MakeCard(PokerPlayer[1], -5, Random.Range(1, 13));
+        
+        MakeCard(PokerCOM[0], 2, Random.Range(1, 13));
+        MakeCard(PokerCOM[1], 5, Random.Range(1, 13));
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("15點");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
